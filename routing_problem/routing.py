072f2fed1870675aed30062fdda5c786dfc6ca74
jsroa15 <js.roa15@gmail.com>
2022-05-02 19:13:44 -0400

    working on model

--- routing_problem/routing.py
+++ routing_problem/routing.py
@@ -6,4 +6,18 @@ from pyomo.environ import *
 from pyomo.opt import SolverFactory
 from data_management import generate_parameters
 
+
+# Generate parameters and data for optimization model
 nodes, edges, distance, v_in, v_out = generate_parameters()
+
+# Create model
+model = pyo.ConcreteModel()
+
+# Create model variables
+model.x = pyo.Var(edges, within=pyo.Binary)
+
+# Create Objective Function
+model.obj = pyo.Objective(expr=sum([distance[i,j]*model.x[i,j] for (i,j) in edges]))
+
+
+
